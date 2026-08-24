59c2223c25e206dacdbc905bafdeb0f0c3d46386
karthirapx <Karthikeyan.R@apexon.com>
2026-08-24 18:57:17 +0530

    fix(security): allow PATCH in CORS config add active field in GET Room API field HotelUpdate is not getting starRating from Update Request

--- stay-ease-server/src/main/java/com/capstone/team40/model/UpdateHotelRequest.java
+++ stay-ease-server/src/main/java/com/capstone/team40/model/UpdateHotelRequest.java
@@ -1,7 +1,8 @@
 package com.capstone.team40.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record UpdateHotelRequest(@NotBlank String name, @NotBlank String city, @NotBlank String description, @NotBlank String coverImageUrl)
+public record UpdateHotelRequest(@NotBlank String name, @NotBlank String city, @NotBlank String description, @NotNull int starRating, @NotBlank String coverImageUrl)
 {
 }
